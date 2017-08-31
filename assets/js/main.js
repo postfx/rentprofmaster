d08a69a8de1d6e2e0d6f88030859c5d7c300b219
valeriy-capt <cherepanov.valeriy@gmail.com>
2017-08-31 19:44:06 +0300

    tovar-bitovki.html 2.11 / 2.14 / 2.16

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,3 +39,24 @@ $('.multiple-items').slick({
   slidesToShow: 2,
   slidesToScroll: 2
 });
+
+$('.types-huts-slider').slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll:2,
+  responsive: [
+
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+});
